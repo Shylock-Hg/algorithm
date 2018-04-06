7d7802bd14645f5ae8b9db543a6ef594e087ae0f
shylock <tcath2s@gmail.com>
2018-04-06 16:22:56 +0800

    remove swap function

--- common/sorting/sort_insert.c
+++ common/sorting/sort_insert.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include <stddef.h>
 
-/*! \brief swap two item value in array
- *  \param a pointer to item in array
- *  \parma b pointer to item in array
- * */
-static inline void swap_item(int * a, int * b){
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
+#include "sort_common.h"
 
 /*! \brief simple insertion sorting algorithm implement
- *  \param array array of unsorted numbers
+ *  \param array[i/o] array of unsorted numbers
  *  \param len lenght of numbers to sort
  *  \note stable
  * */
