b0d9ec937d6df240baf434e8a4a481fea17f20fa
shylock <tcath2s@gmail.com>
2018-04-21 18:40:56 +0800

    avoid repeat alloc by recard pointer in function

--- common/sorting/sort_merge.c
+++ common/sorting/sort_merge.c
@@ -7,16 +7,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "sort_common.h"
 
 static void merge(array_t array, size_t i_start, size_t i_mid, size_t i_end){
-	int * temp = calloc(array.len,sizeof(int));
+	static int * temp = NULL;
+	if(NULL == temp){  //!< alloc once
+		temp = calloc(array.len,sizeof(int));
+	}
 
 	size_t i_left = i_start;
 	size_t i_right = i_mid;
 	size_t i_temp = i_start;
 
-	while(i_left < i_mid || i_right < i_end){
+	while(i_left < i_mid || i_right < i_end){  //!< merge and sort two sub-array to temp
 		if(i_left == i_mid){
 			temp[i_temp] = array.value[i_right];
 			i_right++;
@@ -33,7 +37,7 @@ static void merge(array_t array, size_t i_start, size_t i_mid, size_t i_end){
 		i_temp++;
 	}
 
-	for(i_temp=i_start; i_temp<i_end; i_temp++){
+	for(i_temp=i_start; i_temp<i_end; i_temp++){  //!< update origin sub-array
 		array.value[i_temp] = temp[i_temp];
 	}
 	//memcpy(array.value+i_start,temp+i_start,i_end-i_start);
@@ -43,7 +47,7 @@ static void sort_merge_origin(array_t array, size_t i_start, size_t i_end){
 	if(1 == i_end - i_start)  //!< divide to one element
 		return;
 	
-	int i_mid = i_start + (i_end - i_start)/2;
+	int i_mid = i_start + (i_end - i_start)/2;  //!< divide array
 	sort_merge_origin(array,i_start,i_mid);
 	sort_merge_origin(array,i_mid,i_end);
 
