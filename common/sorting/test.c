c33e29b62aa50cd32ad18b1c2290708342167ffd
shylock <tcath2s@gmail.com>
2018-04-22 14:59:29 +0800

    add negetive numbers

--- common/sorting/test.c
+++ common/sorting/test.c
@@ -13,7 +13,7 @@
 	}while(0);
 
 //! data to sorting 
-int array0[] = {0,1,22,22,3,5,2,99,10000,32,0,32,566,66,66,66,22,33,11,435,657,745,73254,747,467,342,462,21,1346};
+int array0[] = {0,1,-22,22,3,5,2,99,-10000,32,0,-32,566,66,66,66,22,-33,11,435,657,-745,73254,747,467,342,-462,21,-1346};
 
 array_t array = {array0+0,sizeof(array0)/sizeof(array0[0])};
 
@@ -21,9 +21,9 @@ int main(int argc, char * argv[]){
 	printf("origin:\n");
 	println_array(array);
 	
-	//sort_insert(array);
+	sort_insert(array);
 	//sort_select(array);
-	sort_merge(array);
+	//sort_merge(array);
 	//sort_quick(array);
 	
 	printf("sorted:\n");
